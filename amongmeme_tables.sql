1025fd3fd55442ced40328b10500393fc26cff64
pedrofenti <peter8_fg@outlook.com>
2021-02-03 17:36:16 +0000

    script tablas users

--- amongmeme_tables.sql
+++ amongmeme_tables.sql
@@ -9,9 +9,12 @@ DROP TABLE IF EXISTS weapons;
 DROP TABLE IF EXISTS weapons_types;
 
 DROP TABLE IF EXISTS characters_armours;
+DROP TABLE IF EXISTS characters_armors;
 DROP TABLE IF EXISTS armours;
+DROP TABLE IF EXISTS armors;
 DROP TABLE IF EXISTS armours_types;
-
+DROP TABLE IF EXISTS armors_types;
+DROP TABLE IF EXISTS users;
 
 DROP TABLE IF EXISTS skills;
 
@@ -68,7 +71,7 @@ CREATE TABLE items_types(
 );
 INSERT INTO items_types (type)
 VALUES
-	("Otros"),("Equipable"),("Consumible"),("Clave");
+	("Otros"),("Equipable"),("Consumible"),("Clave"),("Especial"),("Municion");
 
 CREATE TABLE items(
 	id_item INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -81,17 +84,18 @@ CREATE TABLE items(
 	description TEXT,
 	id_item_type INT UNSIGNED NOT NULL,
 	FOREIGN KEY (id_item_type) REFERENCES items_types(id_item_type)
+	ON DELETE CASCADE ON UPDATE RESTRICT
 );
-INSERT INTO items (item,cost,consumable,tradeable,weight,image,description,id_item_type)
-VALUES
-	("Poción de vida", 25, true, true, 0.2, "health_potion.png", "Una poción que recupera vida", 3),
-	("Gafas de sol", 100, false, true, 0.1, "sunglasses.png", "Gafas de sol facheritas", 2),
-	("Guijarro", 0, false, false, 0.35, "small_stone.png", "Pequeña piedra que encuentras en el suelo y lo pudes tirar a alguien supongo", 1),
-	("Agua de río", 1000, true, false, 0.5, "river_water.png", "Agua del río con propiedades medicinales que recupera energía de vitalidad", 4),
-	("Manzana", 3, true, true, 0.3, "apple.png", "Manzana Pro",3),
-	("Llave", 25, false, false, 0.1, "key.png", "Llve que abre la puerta del destino", 4),
-	("Gran Anillo", 300, false, true, 0.25, "big_ring.png", "Gran anillo de las profundidades", 2),
-	("Papel", 2, false, true, 0.02, "paper.png", "Papel pequeño en blanco",3);
+INSERT INTO items (item, cost, consumable, weight, tradeable, image, description, id_item_type)
+VALUES ("pocion de vida", 10, true, 1, true, "pocion_vida.png", "pocion que restaura 40hp", 3),
+("pocion de mana", 10, true, 1, true, "pocion_mana.png", "pocion que restaura 100 de mana", 3),
+("gafas de sol", 100, false, 2, true, "gafas_sol.png", "proteccion ante la luz estelar", 2),
+("materializador", 500, false, 15, true, "materializador.png", "objeto capaz de crear materia", 2),
+("asteroide", 500, false, 5, true, "asteroide.png", "asteroide comun que viajaba por el esapcio", 1),
+("pata de Goh", 30, true, 1, true, "pata_goh.png", "resto que suelta un Goh al morir", 4),
+("bombona de O", 999, false, 25, true, "bombona_O.png", "bombona llena de Oxigeno dejandote respirar en el exterior de la nave", 4),
+("traje espacial", 899, false, 20, true, "traje.png", "traje basico para soportar la presion espacial", 4),
+("portatil chungo", 350, false, 2, true, "portatil_chungo.png", "portatil chungo de Intel", 1);
 
 
 CREATE TABLE characters_items (
@@ -100,11 +104,10 @@ CREATE TABLE characters_items (
 	id_item INT UNSIGNED NOT NULL,
 	FOREIGN KEY (id_character) REFERENCES characters(id_character),
 	FOREIGN KEY (id_item) REFERENCES items(id_item)
+	ON DELETE CASCADE ON UPDATE RESTRICT
 );
-INSERT INTO characters_items (id_character,id_item)
-VALUES
-	(1,1),(1,3),(1,4),(2,1),(3,2);
-
+INSERT INTO characters_items (id_character, id_item)
+VALUES (1,1),(1,2),(1,3),(1,4),(1,5),(1,6),(1,7),(1,8),(1,9),(2,1),(2,2),(2,9),(3,1),(3,2),(3,5),(3,6),(4,4),(4,9);
 
 
 CREATE TABLE weapons_types(
@@ -126,6 +129,7 @@ CREATE TABLE weapons(
 	`range` INT NOT NULL,
 	id_weapon_type INT UNSIGNED NOT NULL,
 	FOREIGN KEY (id_weapon_type) REFERENCES weapons_types(id_weapon_type)
+	ON DELETE CASCADE ON UPDATE RESTRICT
 );
 INSERT INTO weapons(weapon, damage, quality, rarity, weight, durability, `range`, id_weapon_type)
 VALUES
@@ -141,36 +145,69 @@ CREATE TABLE characters_weapons(
 	id_weapon INT UNSIGNED NOT NULL,
 	FOREIGN KEY (id_character) REFERENCES characters(id_character),
 	FOREIGN KEY (id_weapon) REFERENCES weapons(id_weapon)
+	ON DELETE CASCADE ON UPDATE RESTRICT
 );
 INSERT INTO characters_weapons (id_character,id_weapon)
 VALUES
 	(1,3), (2,2), (3,4), (4,3);
 
-CREATE TABLE armours_types (
-	id_armour_type INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE armors_types (
+	id_armor_type INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	type VARCHAR(24) NOT NULL
 );
-INSERT INTO armours_types(type)
+INSERT INTO armors_types(type)
 VALUES
 	('Ligera'),('Mediana'),('Pesada');
 
-CREATE TABLE armours(
-	id_armour INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	armour VARCHAR(16) NOT NULL,
+CREATE TABLE armors(
+	id_armor INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	armor VARCHAR(16) NOT NULL,
 	defence INT NOT NULL,
 	durability INT NOT NULL,
 	weight INT NOT NULL,
 	quality INT NOT NULL,
 	rarity INT NOT NULL,
 	toughness INT NOT NULL,
-	id_armour_type INT UNSIGNED NOT NULL,
-	FOREIGN KEY (id_armour_type) REFERENCES armours_types(id_armour_type)
+	id_armor_type INT UNSIGNED NOT NULL,
+	FOREIGN KEY (id_armor_type) REFERENCES armors_types(id_armor_type)
+	ON DELETE CASCADE ON UPDATE RESTRICT
 );
 
-CREATE TABLE characters_armours(
-	id_character_armour INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE characters_armors(
+	id_character_armor INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	id_character INT UNSIGNED NOT NULL,
-	id_armour INT UNSIGNED NOT NULL,
+	id_armor INT UNSIGNED NOT NULL,
 	FOREIGN KEY (id_character) REFERENCES characters(id_character),
-	FOREIGN KEY (id_armour) REFERENCES armours(id_armour)
+	FOREIGN KEY (id_armor) REFERENCES armors(id_armor)
+	ON DELETE CASCADE ON UPDATE RESTRICT
 );
+
+CREATE TABLE users(
+	id_user INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	username VARCHAR(16),
+	name VARCHAR(16),
+	surname VARCHAR(32),
+	birthdate DATE,
+	email VARCHAR(32),
+	password CHAR(32),
+	register TIMESTAMP DEFAULT CURRENT_TIMESTAMP()
+);
+
+DELIMITER //
+
+CREATE PROCEDURE fill_users_table (
+    IN in_username VARCHAR(16),
+    IN in_name VARCHAR(16),    
+    IN in_surname VARCHAR(32),    
+    IN in_birthdate DATE,    
+    IN in_email VARCHAR(32),
+    IN in_password VARCHAR(16) )
+BEGIN
+INSERT INTO users (username, name, surname, birthdate, email, password)
+VALUES ( in_username ,in_name, in_surname, in_birthdate, in_email, MD5(in_password) );
+END //
+
+DELIMITER ;
+
+CALL fill_users_table ('Pedro', 'Bad', 'Bunny', '1996-05-30', 'pedro.fernandez@enti.cat','enti');
+    
